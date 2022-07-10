213199ce86c3bf11d3a1ce0902ea177ff56dca1c
wings <1195372303@qq.com>
2022-07-10 19:09:48 +0800

    update 7.10
    finish forward function and add weights loader

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-test = "hello, how are      you?"
-print(len(test[1:-4]))
+from torch import tensor
+import torch
+import numpy as np
+grid_size = 7
+grid = np.arange(grid_size)
+a,b = np.meshgrid(grid, grid)
+
+x_offset = torch.FloatTensor(a).view(-1,1)
+y_offset = torch.FloatTensor(b).view(-1,1)
+
+x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1,3).view(-1,2)
+
+print(x_y_offset)
